64dfa1a02d43583c3d59422555f22d8f246b4ef6
Max Gonzih <gonzih@gmail.com>
2017-12-03 16:45:53 +0100

    Init

---
+++ src/glue_demo/core.cljs
@@ -0,0 +1,32 @@
+(ns glue-demo.core
+  (:refer-clojure :exclude [atom])
+  (:require [glue.core :as g :refer [atom]]))
+
+(enable-console-print!)
+
+(g/defcomponent
+  :todo
+  {:template "#todo"
+   :state (fn [] {:todos   (atom ["hello"])
+                  :counter (atom 0)})
+   :methods {:add-random-todo (fn [this state _]
+                                (swap! (:todos state) #(conj % (str (rand-int 100)))))
+             :child-clicked (fn [this state n]
+                              (swap! (:counter state) inc))}})
+
+(g/defcomponent
+  :todo-item
+  {:template "#todo-item"
+   :props [:label]
+   :state (fn [] {:counter (atom 0)})
+   :computed {:counter-label (fn [this state]
+                               (str @(:counter state) " clicks"))}
+   :methods {:click-me (fn [this state _]
+                         (println "Click happened on" (g/prop this :label))
+                         (swap! (:counter state) inc)
+                         (g/emit this :todo-click 1))}})
+
+(defonce app (g/vue {:el "#app"}))
+
+(defn on-js-reload []
+  (g/reset-state!))
